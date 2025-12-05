5adb0324b0d554b44b48d2a3b26045e88cac719e
luis-03-2004 <luis.monteiro@alunos.ifsuldeminas.edu.br>
2025-12-04 23:22:42 -0300

    correcao para funcionar o secret manager

--- database/connection.py
+++ database/connection.py
@@ -1,18 +1,26 @@
-# app/database.py
+# app/database/connection.py
 import os
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, declarative_base
+from config import load_secrets # <--- Importe sua função
 
-# As variáveis de ambiente são carregadas no main.py via AWS Secrets Manager ou .env
+# --- CORREÇÃO AQUI ---
+# Antes de tentar criar a engine, verifique se a URL existe.
+# Se não existir (ou for vazia), force o carregamento dos segredos da AWS.
+if not os.getenv("DATABASE_URL"):
+    load_secrets() 
+
 SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
+
+# Proteção extra: Se ainda assim estiver vazio, avisa o erro
+if not SQLALCHEMY_DATABASE_URL:
+    raise ValueError("A variavel DATABASE_URL esta vazia. Verifique o Secrets Manager.")
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
 
-# Dependência para obter a sessão do banco de dados
 def get_db():
     db = SessionLocal()
     try:
